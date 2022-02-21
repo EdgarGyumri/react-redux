dbea1cce2a015303ee94b5c939261096701a4be1
Edgar Gharibyan <edgharibyan1@gmail.com>
2022-02-21 11:31:26 +0400

    init project

---
+++ src/store/reducer.js
@@ -0,0 +1,26 @@
+export default function reducer(state = [], action) {
+  switch(action.type){
+    case 'addUser':
+      return [
+        ...state,
+        {
+          name: action.payload.name,
+          surname: action.payload.surname,
+          rate: []
+        }
+      ]
+    case 'rateUser':
+      if(action.payload.rate !== undefined){
+        state[action.payload.id].rate.push(action.payload.rate);
+      }
+      return [
+        ...state
+      ]
+
+    default:
+      return [
+        ...state
+      ]
+
+  }
+}
